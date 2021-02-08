b1589c53519a4625476fbff21d5fcc3cf1d3d7a0
Zikai Cai <zikaicai@usc.edu>
2021-02-07 20:55:16 -0800

    Initial commit

---
+++ Assets/Scripts/BallSpawner.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallSpawner : MonoBehaviour
+{
+
+    public GameObject ball_Prefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnBall();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SpawnBall()
+    {
+        GameObject ball_Obj = Instantiate(ball_Prefab);
+
+        Vector3 temp = transform.position;
+        ball_Obj.transform.position = temp;
+		
+		ball_Obj = Instantiate(ball_Prefab);
+
+        temp = transform.position;
+		temp.x = temp.x + 1f;
+        ball_Obj.transform.position = temp;
+    }
+}
---
+++ Assets/Scripts/HolderSpawner.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HolderSpawner : MonoBehaviour
+{
+	
+    public GameObject holder_Prefab;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnHolder();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+	
+    public void SpawnHolder()
+    {
+		float x, y;
+		for(x=-11;x<=11;x+=0.1f)
+		{
+			for(y=-4;y<=4;y+=0.1f)
+			{
+				GameObject holder_Obj = Instantiate(holder_Prefab);
+
+				Vector3 temp;
+				temp.x = x;
+				temp.y = y;
+				temp.z = 0f;
+				holder_Obj.transform.position = temp;
+			}
+		}
+    }
+}

cc7d584fef23394595ed15e368920a4b78b09ab2
Zikai Cai <zikaicai@usc.edu>
2021-02-09 14:15:53 -0800

    Format document

--- Assets/Scripts/HolderSpawner.cs
+++ Assets/Scripts/HolderSpawner.cs
@@ -4,30 +4,30 @@ using UnityEngine;
 
 public class HolderSpawner : MonoBehaviour {
 
-  public GameObject holder_Prefab;
+	public GameObject holder_Prefab;
 
-  // Start is called before the first frame update
-  void Start () {
-    SpawnHolder ();
-  }
+	// Start is called before the first frame update
+	void Start () {
+		SpawnHolder ();
+	}
 
-  // Update is called once per frame
-  void Update () {
+	// Update is called once per frame
+	void Update () {
 
-  }
+	}
 
-  public void SpawnHolder () {
-    float x, y;
-    for (x = -11; x <= 11; x += 0.1f) {
-      for (y = -4; y <= 4; y += 0.1f) {
-        GameObject holder_Obj = Instantiate (holder_Prefab);
+	public void SpawnHolder () {
+		float x, y;
+		for (x = -11; x <= 11; x += 0.1f) {
+			for (y = -4; y <= 4; y += 0.1f) {
+				GameObject holder_Obj = Instantiate (holder_Prefab);
 
-        Vector3 temp;
-        temp.x = x;
-        temp.y = y;
-        temp.z = 0f;
-        holder_Obj.transform.position = temp;
-      }
-    }
-  }
+				Vector3 temp;
+				temp.x = x;
+				temp.y = y;
+				temp.z = 0f;
+				holder_Obj.transform.position = temp;
+			}
+		}
+	}
 }

b711ce7747e115a1c964ed0f698620c7b2fbdc8f
Zikai Cai <zikaicai@usc.edu>
2021-02-09 14:12:46 -0800

    Format document

--- Assets/Scripts/BallSpawner.cs
+++ Assets/Scripts/BallSpawner.cs
@@ -2,34 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BallSpawner : MonoBehaviour
-{
+public class BallSpawner : MonoBehaviour {
 
     public GameObject ball_Prefab;
 
     // Start is called before the first frame update
-    void Start()
-    {
-        SpawnBall();
+    void Start () {
+        SpawnBall ();
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update () {
+
     }
 
-    public void SpawnBall()
-    {
-        GameObject ball_Obj = Instantiate(ball_Prefab);
+    public void SpawnBall () {
+        GameObject ball_Obj = Instantiate (ball_Prefab);
 
         Vector3 temp = transform.position;
         ball_Obj.transform.position = temp;
-		
-		ball_Obj = Instantiate(ball_Prefab);
+
+        ball_Obj = Instantiate (ball_Prefab);
 
         temp = transform.position;
-		temp.x = temp.x + 1f;
+        temp.x = temp.x + 1f;
         ball_Obj.transform.position = temp;
     }
 }
--- Assets/Scripts/HolderSpawner.cs
+++ Assets/Scripts/HolderSpawner.cs
@@ -2,38 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HolderSpawner : MonoBehaviour
-{
-	
-    public GameObject holder_Prefab;
-	
-    // Start is called before the first frame update
-    void Start()
-    {
-        SpawnHolder();
-    }
+public class HolderSpawner : MonoBehaviour {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-	
-    public void SpawnHolder()
-    {
-		float x, y;
-		for(x=-11;x<=11;x+=0.1f)
-		{
-			for(y=-4;y<=4;y+=0.1f)
-			{
-				GameObject holder_Obj = Instantiate(holder_Prefab);
+  public GameObject holder_Prefab;
 
-				Vector3 temp;
-				temp.x = x;
-				temp.y = y;
-				temp.z = 0f;
-				holder_Obj.transform.position = temp;
-			}
-		}
+  // Start is called before the first frame update
+  void Start () {
+    SpawnHolder ();
+  }
+
+  // Update is called once per frame
+  void Update () {
+
+  }
+
+  public void SpawnHolder () {
+    float x, y;
+    for (x = -11; x <= 11; x += 0.1f) {
+      for (y = -4; y <= 4; y += 0.1f) {
+        GameObject holder_Obj = Instantiate (holder_Prefab);
+
+        Vector3 temp;
+        temp.x = x;
+        temp.y = y;
+        temp.z = 0f;
+        holder_Obj.transform.position = temp;
+      }
     }
+  }
 }

27f985555a9573838945c0030c5df648bf8c88e7
Wenhao Ge <wge1@ucsc.edu>
2021-03-08 22:45:04 -0800

    Update Destination.cs

--- Assets/Scripts/Destination.cs
+++ Assets/Scripts/Destination.cs
@@ -6,10 +6,12 @@ using UnityEngine.Analytics;
 public class Destination : MonoBehaviour
 {
     private DestinationController dc;
+    private LevelOneManager levelOneManager;
     // Start is called before the first frame update
     void Start()
     {
         dc = FindObjectOfType<DestinationController> ();
+        levelOneManager = FindObjectOfType<LevelOneManager>();
     }
 
     // Update is called once per frame
@@ -22,8 +24,23 @@ public class Destination : MonoBehaviour
 
         Destroy (other.gameObject);
         dc.numBall -= 1;
-        AnalyticsResult analyticsResult = Analytics.CustomEvent("LevelWin" + (2 - dc.numBall));
+        AnalyticsResult analyticsResult = Analytics.CustomEvent(
+        	"LevelWin",
+        	new Dictionary<string, object> {
+        		{"Level", 1},
+        		{"NumBallToWin", (2 - dc.numBall)}
+        	}
+        );
         Debug.Log("analyticsResult: " + analyticsResult);
+
+        int levelScore = levelOneManager.score;
+        Analytics.CustomEvent(
+            "DiamondCollect",
+            new Dictionary<string, object> {
+                {"Level", 1},
+                {"NumDiamondCollect", levelScore}
+            }
+        );
 
         if(dc.numBall == 0)  Application.Quit();
     }

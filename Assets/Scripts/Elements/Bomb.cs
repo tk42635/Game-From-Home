04d24f59c6ea21ad49affe2d3e7b91b4d0f7d043
zhixingc <smal1thing84@gmail.com>
2021-03-30 11:40:15 +0800

    add some sound effect and change the bgm

--- Assets/Scripts/Elements/Bomb.cs
+++ Assets/Scripts/Elements/Bomb.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 
 public class Bomb : MonoBehaviour {
     private LevelManager levelManager;
+    public AudioClip boomAudioClip;
+    public AudioClip gameOverAudioClip;
     // Start is called before the first frame update
     void Start () {
         levelManager = FindObjectOfType<LevelManager> ();
+        boomAudioClip = Resources.Load<AudioClip>(ResourcesPath.BOOM_AUDIO_PATH);
+        gameOverAudioClip = Resources.Load<AudioClip>(ResourcesPath.GAME_OVER_AUDIO_PATH);
     }
 
     // Update is called once per frame
@@ -17,6 +21,8 @@ public class Bomb : MonoBehaviour {
     void OnTriggerEnter2D (Collider2D other) {
         Destroy (other.gameObject);
         Destroy (gameObject);
+        AudioSource.PlayClipAtPoint(boomAudioClip, transform.position, 100);
+        AudioSource.PlayClipAtPoint(gameOverAudioClip, transform.position, 100);
         // levelManager.levelBallExist -= 1;
         // if (levelManager.levelBallExist == 0)
         levelManager.LevelDone ();

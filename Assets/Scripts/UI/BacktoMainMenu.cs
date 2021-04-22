2475a5d9840d1e8294532797ab029984c0db92f7
Zikai Cai <zikaicai@usc.edu>
2021-04-22 10:25:12 -0700

    Add back button

---
+++ Assets/Scripts/UI/BacktoMainMenu.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BacktoMainMenu : MonoBehaviour {
+    // Start is called before the first frame update
+    void Start () {
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+    }
+
+    public void OnMouseDown () {
+        SceneManager.LoadScene ("Menu");
+    }
+}

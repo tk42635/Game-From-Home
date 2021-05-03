d24c7690bdb484fbfcc75403dd62ef70f97c548b
zhixingc <smal1thing84@gmail.com>
2021-05-04 00:35:04 +0800

    add more sound effect

--- Assets/Scripts/ResourcesPath.cs
+++ Assets/Scripts/ResourcesPath.cs
@@ -2,5 +2,7 @@ public class ResourcesPath {
     public const string DIAMONDS_AUDIO_PATH = "Bonus";
     public const string BOOM_AUDIO_PATH = "Explosion";
     public const string GAME_OVER_AUDIO_PATH = "GameOver";
-
+    public const string HIT_PLANK_AUDIO_PATH = "Spawn";
+    public const string CLICK_PATH = "Click";
+    public const string WHOOSH_PATH = "Whoosh";
 }
